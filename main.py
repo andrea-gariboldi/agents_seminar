5c6a1e397a549529b0c39d876a904eea8cd51c91
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2026-01-27 19:03:02 +0100

    Temp main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic_ai.providers.ollama import OllamaProvider
 
 from tools.bash_tool import create_bash_tool
 from utils.agent_utils import run_agent
+from eval.evaluate_result import evaluate_clustering
 
 ollama_model = OpenAIChatModel(
     model_name='mistral:7b',
@@ -16,18 +17,20 @@ ollama_model = OpenAIChatModel(
 tools = [
     create_bash_tool(runs_dir=os.getcwd(), 
                      timeout=60, 
-                     max_retries=2) 
+                     max_retries=2, conda_env_path="/home/agari01/miniconda3/envs/agents_env") 
                      #conda_env_path needs to be set according to your local env 
     ]
 
 agent = Agent(
     ollama_model,
     tools=tools,
-    system_prompt="You are an sysadmin expert",
+    system_prompt="You are an expert agent with bash scripting skills. Use the tools provided to answer the user's requests.",
     deps_type=str
 )
 
-user_prompt = """Return me the list of files from the current directory in a human friendly way"""
+user_prompt = """Your final deliverable is to cluster a datasets. The sumbmission file should contain the original columns: feat1, feat2, feat3, feat4 plus a new column cluster_id indicating the cluster assignment for each row.
+            The original dataset is: /home/agari01/agents_seminar/dataset.csv. Save the file as submission.csv in the current working directory.            
+            """
 
 async def main():
     await run_agent(
@@ -36,5 +39,7 @@ async def main():
         max_steps=10,
         deps=None
     )
+
+    evaluate_clustering(submission_path="/home/agari01/agents_seminar/submission.csv")
 
 asyncio.run(main())
