f68aa86048164850474f9ba079cfee457e6d0a02
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2026-01-27 17:38:25 +0100

    Add agent run utils

---
+++ utils/agent_utils.py
@@ -0,0 +1,24 @@
+import traceback
+
+from pydantic_ai import Agent, capture_run_messages
+from pydantic_ai.usage import UsageLimits
+
+from utils.printing_utils import print_agent_node as pretty_print_node
+
+async def run_agent(agent: Agent, user_prompt: str, max_steps: int, deps=None):
+    with capture_run_messages():
+        try:
+            async with agent.iter(
+                user_prompt=user_prompt,
+                usage_limits=UsageLimits(request_limit=max_steps),
+                deps=deps,
+            ) as agent_run:
+                async for node in agent_run:
+                    pretty_print_node(node)
+                return agent_run.result.all_messages(), agent_run.result.output
+
+        except Exception as e:
+            trace = traceback.format_exc()
+            print('--------------- ERROR TRACEBACK ---------------')
+            print('Agent run failed', trace)
+            print('--------------- ERROR TRACEBACK ---------------')
