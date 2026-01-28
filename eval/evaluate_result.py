6a5ef6fb93da28538b14cfb8c4070beefc0639c4
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2026-01-28 14:57:46 +0100

    Improve printing when evaluating

--- eval/evaluate_result.py
+++ eval/evaluate_result.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 
 def evaluate_clustering(submission_path: str):
+    print("==========Running Clustering Evaluation==========")
     try: 
         submission_df = pd.read_csv(submission_path)
         
@@ -15,5 +16,7 @@ def evaluate_clustering(submission_path: str):
         print(f"Silhouette Score: {silhouette:.3f} (higher is better, max=1)")
         print(f"Davies-Bouldin Index: {davies_bouldin:.3f} (lower is better, min=0)")
         print(f"Calinski-Harabasz Score: {calinski_harabasz:.2f} (higher is better)")
+        print("==========Clustering Evaluation Finished==========")
     except Exception as e:
-        print("Error calculating metrics", e)
+        print("==========Clustering Evaluation Failed==========")
+        print(e)
