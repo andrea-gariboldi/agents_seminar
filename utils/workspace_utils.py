91901a462b7e861bd878609fe2f6ab4e65acae1b
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2026-02-02 14:51:59 +0000

    Update workspace removal to delete folders

--- utils/workspace_utils.py
+++ utils/workspace_utils.py
@@ -5,4 +5,16 @@ def cleanup_workspace(workspace_dir: str = "agents_workspace"):
 		if item.is_dir() and item.name == "data":
 			continue
 		else:
-			item.unlink()
+			if item.is_dir():
+				rmdir(item)
+			else:
+				item.unlink()
+
+def rmdir(directory):
+    directory = Path(directory)
+    for item in directory.iterdir():
+        if item.is_dir():
+            rmdir(item)
+        else:
+            item.unlink()
+    directory.rmdir()
